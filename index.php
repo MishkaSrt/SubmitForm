59d637bf7bdf68388a2fd7bf2c55b4a6c1510fb6
unknown <mishka.sxirtladze@gmail.com>
2022-06-20 11:42:52 -0700

    submitform

--- index.php
+++ index.php
@@ -3,35 +3,35 @@ include 'create.php';
 
 // Input validation section
 
-$fname = $lname = $email = "";
-$fnameErr = $lnameErr = $emailErr = "";
+// $fname = $lname = $email = "";
+// $fnameErr = $lnameErr = $emailErr = "";
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if(empty($_POST["firstname"])){
-        $fnameErr = "Name is required";
-    } else{
-        $fname = test_input($_POST["firstname"]);
-    }
+// if($_SERVER["REQUEST_METHOD"] == "POST"){
+//     if(empty($_POST["firstname"])){
+//         $fnameErr = "Name is required";
+//     } else{
+//         $fname = test_input($_POST["firstname"]);
+//     }
 
-    if(empty($_POST["lastname"])){
-        $lnameErr = "Surname is required";
-    }else{
-        $lname = test_input($_POST["lastname"]);
-    }
+//     if(empty($_POST["lastname"])){
+//         $lnameErr = "Surname is required";
+//     }else{
+//         $lname = test_input($_POST["lastname"]);
+//     }
 
-    if(empty($_POST["email"])){
-        $emailErr = "Email is required";
-    } else{
-        $email = test_input($_POST["email"]);
-    }
-}
+//     if(empty($_POST["email"])){
+//         $emailErr = "Email is required";
+//     } else{
+//         $email = test_input($_POST["email"]);
+//     }
+// }
 
-function test_input($data){
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+// function test_input($data){
+//     $data = trim($data);
+//     $data = stripslashes($data);
+//     $data = htmlspecialchars($data);
+//     return $data;
+// }
 
 ?>
 
@@ -53,18 +53,20 @@ function test_input($data){
     <div>
         <div id="message"></div>
     </div>
+
+
 <div class="formDiv">
         <form action="" method="post" class="form" id="form">
             <p class="signup">Sign Up</p>
             <label for="fname">First Name</label>
             <input type="text" name="fname" id="fname" placeholder="Enter your name">
-            <span class="error">*<?php echo $fnameErr; ?></span>
+            <!-- <span class="error">*<?php echo $fnameErr; ?></span> -->
             <label for="lname">Last Name</label>
             <input type="text" name="lname" id="lname" placeholder="Enter your surname">
-            <span class="error">*<?php echo $lnameErr; ?></span>
+            <!-- <span class="error">*<?php echo $lnameErr; ?></span> -->
             <label for="email">Email</label>
             <input type="text" name="email" id="email" placeholder="Enter your email">
-            <span class="error">*<?php echo $emailErr; ?></span>
+            <!-- <span class="error">*<?php echo $emailErr; ?></span> -->
             <button type="submit" id="submit" name="submit">Send</button>
             <button ><a href="view.php" >See List</a></button>
 
--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-$(document).on('click', '#submit', function(e){
-    e.preventDefault();
+// $(document).on('click', '#submit', function(e){
+//     e.preventDefault();
 
-    var fname = $('#fname').val();
-    var lname = $('#lname').val();
-    var email = $('#email').val();
-    var submit = $('#submit').val();
+//     var fname = $('#fname').val();
+//     var lname = $('#lname').val();
+//     var email = $('#email').val();
+//     var submit = $('#submit').val();
 
-    $.ajax({
-        url: 'create.php',
-        type: 'post',
-        data: {
-            firstname : fname,
-            lastname : lname,
-            email : email,
-            submit: submit
-        },
-        success: function(response){
-            // fetch();
-            $('#message').html(response);
-        }
+//     $.ajax({
+//         url: 'create.php',
+//         type: 'post',
+//         data: {
+//             firstname : fname,
+//             lastname : lname,
+//             email : email,
+//             submit: submit
+//         },
+//         success: function(response){
+//             // fetch();
+//             $('#message').html(response);
+//         }
+//     });
+
+//     $('#form')[0].reset();
+// });
+
+
+
+$(document).ready(function (){
+    $('#form').submit(function (e){
+        e.preventDefault();
+
+        var fname = $('#fname').val();
+        var lname = $('#lname').val();
+        var email = $('#email').val();
+        var submit = $('#submit').val();
+
+        $.ajax({
+            url : 'create.php',
+            type: 'post',
+            data : {
+                firstname : fname,
+                lastname : lname,
+                email : email,
+                submit : submit
+            },
+            success: function(response){
+                $('#message').html(response)
+            }
+        });
+
+        $('#form')[0].reset();
     });
-
-    $('#form')[0].reset();
 });
